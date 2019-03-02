2fb1f56fa5b807a72b7e046ec294f67d15caee51
florimondmanca <florimond.manca@gmail.com>
2019-03-02 16:17:48 +0100

    test: generator provider factory

--- tests/test_provider_factories.py
+++ tests/test_provider_factories.py
@@ -18,3 +18,27 @@ async def test_provider_factory_pattern(store: Store):
         return await note(pk)
 
     assert await get_note(10) == {"id": 10}
+
+
+async def test_generator_provider_factory(store: Store):
+    setup = False
+    cleanup = False
+
+    @store.provider
+    async def note():
+        nonlocal setup, cleanup
+        setup = True
+
+        async def _get_note(pk: int):
+            return {"id": pk}
+
+        yield _get_note
+        cleanup = True
+
+    @store.consumer
+    async def get_note(pk: int, note):
+        return await note(pk)
+
+    assert await get_note(10) == {"id": 10}
+    assert setup
+    assert cleanup
