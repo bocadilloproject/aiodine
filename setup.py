7731e812b18824c089b1fee4f5efb96de841fe40
Florimond Manca <florimond.manca@gmail.com>
2019-09-12 23:14:53 +0200

    Add Python 3.8 support (#36)
    * Add 3.8-dev to CI build
    
    * Add 3.8 trove classifier

--- setup.py
+++ setup.py
@@ -27,12 +27,13 @@ setuptools.setup(
     url=GITHUB,
     license="MIT",
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
