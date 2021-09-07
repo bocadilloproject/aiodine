1f0b3896f17a58a00b2dfb8e3b34e98fbb046a92
Oleg A <t0rr@mail.ru>
2021-09-07 07:33:10 +0300

    chore: python3.8 support

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Framework :: AsyncIO",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
